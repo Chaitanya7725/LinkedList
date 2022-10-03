95dc44ad96678aaaf3a28bf3b5ec3236a3831577
Chaitanya7725 <chaitanya7725@gmail.com>
2022-10-02 20:59:04 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        LL ll=new LL();
+        ll.insert(5);
+        ll.insert(17);
+        ll.insert(32);
+        ll.insert(4);
+    }
+}
